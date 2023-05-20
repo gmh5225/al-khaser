dfe9fa5afde9e6b99783ff9fbb47214ee8568ddd
gmh5225 <2315157@qq.com>
2023-05-20 21:29:56 +0800

    1

--- al-khaser/AntiVM/HyperV.cpp
+++ al-khaser/AntiVM/HyperV.cpp
@@ -13,26 +13,32 @@ BOOL check_hyperv_driver_objects()
 	{
 		if (StrCmpCW(driver, L"VMBusHID") == 0)
 		{
+			printf("check_hyperv_driver_objects: VMBusHID\n");
 			return TRUE;
 		}
 		if (StrCmpCW(driver, L"vmbus") == 0)
 		{
+			printf("check_hyperv_driver_objects: vmbus\n");
 			return TRUE;
 		}
 		if (StrCmpCW(driver, L"vmgid") == 0)
 		{
+			printf("check_hyperv_driver_objects: vmgid\n");
 			return TRUE;
 		}
 		if (StrCmpCW(driver, L"IndirectKmd") == 0)
 		{
+			printf("check_hyperv_driver_objects: IndirectKmd\n");
 			return TRUE;
 		}
 		if (StrCmpCW(driver, L"HyperVideo") == 0)
 		{
+			printf("check_hyperv_driver_objects: HyperVideo\n");
 			return TRUE;
 		}
 		if (StrCmpCW(driver, L"hyperkbd") == 0)
 		{
+			printf("check_hyperv_driver_objects: hyperkbd\n");
 			return TRUE;
 		}
 	}
@@ -51,20 +57,26 @@ BOOL check_hyperv_global_objects()
 	{
 		if (StrStrW(globalObj, L"VMBUS#") != NULL)
 		{
+			printf("check_hyperv_global_objects: VMBUS#\n");
 			return TRUE;
 		}
 		if (StrCmpCW(globalObj, L"VDRVROOT") == 0)
 		{
+			printf("check_hyperv_global_objects: VDRVROOT\n");
 			return TRUE;
 		}
 		if (StrCmpCW(globalObj, L"VmGenerationCounter") == 0)
 		{
+			printf("check_hyperv_global_objects: VmGenerationCounter\n");
 			return TRUE;
 		}
 		if (StrCmpCW(globalObj, L"VmGid") == 0)
 		{
+			printf("check_hyperv_global_objects: VmGid\n");
 			return TRUE;
 		}
 	}
 	return FALSE;
 }
+
+
